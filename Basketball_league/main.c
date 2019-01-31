8b13eb8df5a2ff6831e4412a4c3c74b4ac9553e3
MilanBojic1999 <mbojic12@gmail.com>
2019-01-31 22:40:10 +0100

    Change 31. I 2019.
    Now you can play games with two selected( by user) teams from table
    It is possible to delete team from table

--- Basketball_league/main.c
+++ Basketball_league/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 typedef struct T{
     char naziv[25];
     int napad;
@@ -20,7 +21,7 @@ Tim* napravi_tim(char naziv[],int napad,int odbrana){
 void Info_meni(){
     printf("1) Ucitaj iz fajla\n");
     printf("2) Dodaj na fajl timove\n");
-    printf("3) Prepisi preko fajla\n");
+    printf("3) Obrisi tim\n");
     printf("4) Odigraj mec\n");
     printf("0) Vidi rezultate");
     printf("\n");
@@ -41,15 +42,59 @@ Tim* napravi_ligu(Tim* stablo,Tim* novi){
     return stablo;
 }
 
+Tim* obrisi_tim(Tim* lista,int n){ //brise n-ti tim
+    Tim* zaBrisanje,*tmp;
+    if(n==1){
+        zaBrisanje=lista;
+        lista=lista->sledeci;
+        free(zaBrisanje);
+        return lista;
+    }
+    int i;
+    tmp=lista;
+    for(i=2;i<n && tmp->sledeci;i++){
+        tmp=tmp->sledeci;
+    }
+    zaBrisanje=tmp->sledeci;
+    tmp->sledeci=zaBrisanje->sledeci;
+    free(zaBrisanje);
+    return lista;
+}
+
+void odigraj_mec(Tim* tim1,Tim* tim2){
+    int i,rez1=0,rez2=0;
+    int tabela[4][10]={ {0,0,0,1,1,2,2,2,2,3},
+                    {0,0,1,1,2,2,2,2,3,3},
+                    {0,0,1,2,2,2,2,3,3,3},
+                    {0,1,2,2,2,2,3,3,3,4}   };
+    int napad1=tim1->napad-tim2->odbrana+50;
+    int napad2=tim2->napad-tim1->odbrana+50;
+    printf("%d %d\n",napad1,napad2);
+    srand(time(0));
+    for(i=0;i<100;i++){
+        if(i%2){
+            rez1+=tabela[napad1/25][rand()%10];
+        }else{
+            rez2+=tabela[napad2/25][rand()%10];
+        }
+        printf("%d : %d\n",rez1,rez2);
+    }
+}
+
 void info(char *ime,int *n,int *o){
     printf("Kako se zove vas tim? ");
     scanf(" %[^\n]",ime);
     printf("Kakav je napad tima? ");
     scanf("%d",n);
+    if(*n<50&& *n!=0) *n=50;
+    else if(*n>100) *n=50;
     printf("Kakva je odbrana time? ");
     scanf("%d",o);
+    if(*o<50 && *o!=0) *o=50;
+    else if(*o>100) *o=50;
     printf("\n");
 }
+
 Tim* otvori_fajl(){
     Tim* lista=NULL;
     char *token,naziv[25],linija[100];
@@ -68,8 +113,23 @@ Tim* otvori_fajl(){
     return lista;
 }
 
+Tim* izaberite_tim(Tim* lista){
+    int n,i;
+    ispisi(lista);
+    printf("Koji tim birate? ");
+    scanf("%d",&n);
+    if(n==1){
+        return lista;
+    }
+    Tim* tmp=lista;
+    for(i=1;i<n && tmp->sledeci;i++){
+        tmp=tmp->sledeci;
+    }
+    return tmp;
+}
+
 void dodaj_na_fajl(Tim* stablo){
-    FILE* f=fopen("Kosarka.txt","a");
+    FILE* f=fopen("Kosarka.txt","w");
     while(stablo){
         fprintf(f,"%s - %d:%d\n",stablo->naziv,stablo->napad,stablo->odbrana);
         stablo=stablo->sledeci;
@@ -88,7 +148,7 @@ Tim* dodaj_u_listu(Tim* predstavnik){
     return predstavnik;
 }
 
-void obrisi(Tim* tabela){
+void obrisi(Tim* tabela){ //brise citavu listu
     Tim* zaBrisanje;
     while(tabela){
         zaBrisanje=tabela;
@@ -108,19 +168,33 @@ void ispisi(Tim* stablo){
 int main()
 {
     Tim* predstavnik=NULL;
-    int n;
+    int n,k;
     do{
         Info_meni();
         scanf("%d",&n);
         switch(n){
             case 1:
-                predstavnik=otvori_fajl();
+                predstavnik=otvori_fajl(); //uèitava iz fajla Kosarka
                 break;
             case 2:
-                predstavnik=dodaj_u_listu(predstavnik);
+                predstavnik=dodaj_u_listu(predstavnik); //ubacivanje novih timova
                 dodaj_na_fajl(predstavnik);
                 break;
-
+            case 3:{
+                ispisi(predstavnik);
+                scanf("%d",&k);
+                predstavnik=obrisi_tim(predstavnik,k);//brise tim iz liste
+                dodaj_na_fajl(predstavnik);
+                break;
+            }
+            case 4:{
+                Tim* tim1,*tim2;
+                tim1=izaberite_tim(predstavnik);
+                tim2=izaberite_tim(predstavnik);
+                printf("%s %s\n",tim1->naziv,tim2->naziv);
+                odigraj_mec(tim1,tim2);
+                break;
+            }
         }
     }while(n);
     ispisi(predstavnik);
