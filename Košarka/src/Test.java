8d5a352ea192840774184ed3331bb598011699b0
MilanBojic1999 <mbojic12@gmail.com>
2019-03-21 19:20:54 +0100

    21.3.2019
    From this version, after playing match table will be sorted out in descending order

--- Košarka/src/Test.java
+++ Košarka/src/Test.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Test {
-   public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        Tim zvezda=new Tim("Crvena zvezda",85,90);
-        Tim rad=new Tim("Rad",78,66);
-        Tim OFKa=new Tim("OFK Beograd",64,70);
-        Tim part=new Tim("Partizan",78,91);
-        Tim vojv=new Tim("Vojvodina",75,75);
-        Tim mega=new Tim("Mega Leks",83,69);
-        Tim mor=new Tim("Mornar",50,99);
+        Tim zvezda = new Tim("Crvena zvezda", 85, 90);
+        Tim rad = new Tim("Rad", 78, 66);
+        Tim OFKa = new Tim("OFK Beograd", 64, 70);
+        Tim part = new Tim("Partizan", 78, 91);
+        Tim vojv = new Tim("Vojvodina", 75, 75);
+        Tim mega = new Tim("Mega Leks", 83, 69);
+        Tim mor = new Tim("Mornar", 50, 99);
 
-        Liga jelenSuper=new Liga("Jelen super liga",10);
+        Liga jelenSuper = new Liga("Jelen super liga", 10);
 
         jelenSuper.dodajTim(zvezda);
         jelenSuper.dodajTim(rad);
--- Košarka/src/Tim.java
+++ Košarka/src/Tim.java
@@ -1,4 +1,4 @@
-public class Tim {
+public class Tim implements Comparable<Tim> {
     private String naziv;
     private int napad;
     private int odbrana;
@@ -6,34 +6,56 @@ public class Tim {
     private int primljeniKosevi;
     private int bodovi;
 
-    public Tim(String naziv,int napad,int odbrana){
-        this.naziv=naziv;
-        this.napad=napad;
-        this.odbrana=odbrana;
-        this.datiKosevi=0;
-        this.primljeniKosevi=0;
-        this.bodovi=0;
+    public Tim(String naziv, int napad, int odbrana) {
+        this.naziv = naziv;
+        this.napad = napad;
+        this.odbrana = odbrana;
+        this.datiKosevi = 0;
+        this.primljeniKosevi = 0;
+        this.bodovi = 0;
     }
-    public int getNapad(){
+
+    public int getNapad() {
 
         return this.napad;
     }
-    public int getOdbrana(){
+
+    public int getOdbrana() {
 
         return this.odbrana;
     }
-    public void dodajRezultat(int bodovi,int dKos,int pKos){
-        this.bodovi+=bodovi;
-        this.datiKosevi+=dKos;
-        this.primljeniKosevi+=pKos;
+
+    public int getBodovi(){
+        return this.bodovi;
     }
+
+    public int getRazlikaKoseva(){
+        return this.datiKosevi-this.primljeniKosevi;
+    }
+
+    public void dodajRezultat(int bodovi, int dKos, int pKos) {
+        this.bodovi += bodovi;
+        this.datiKosevi += dKos;
+        this.primljeniKosevi += pKos;
+    }
+
     @Override
     public String toString() {
-        return this.naziv+" "+this.napad+":"+this.odbrana;
-    }
-    public String dajRezultate(){
-
-        return this.naziv+"\t| "+this.datiKosevi+":"+this.primljeniKosevi+" - "+this.bodovi;
+        return this.naziv + " " + this.napad + ":" + this.odbrana;
     }
 
+    public String dajRezultate() {
+
+        return this.naziv + "\t| " + this.datiKosevi + ":" + this.primljeniKosevi + " - " + this.bodovi;
+    }
+
+    @Override
+    public int compareTo(Tim tim) {
+        if(this.bodovi>tim.getBodovi()) return -1;
+        else if(this.bodovi<tim.getBodovi()) return 1;
+        else {
+            if((this.getRazlikaKoseva())>tim.getRazlikaKoseva()) return -1;
+            else return 1;
+        }
+    }
 }
